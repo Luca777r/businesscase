c4759bc27366f44d4d734a57567d30dbe7df8b32
Lucius <lucadelrosso.pro@gmail.com>
2022-01-24 13:38:33 +0100

    page de connexion dashboard done

--- Dashboard Angular/DashboardAngular/src/app/app-routing.module.ts
+++ Dashboard Angular/DashboardAngular/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageConnexionComponent } from './Pages/page-connexion/page-connexion.component';
+import { MonGuardGuard } from './Pages/page-dash-board/mon-guard.guard';
 import { PageDashBoardComponent } from './Pages/page-dash-board/page-dash-board.component';
 
 const routes: Routes = [
   { path: 'Connexion', component: PageConnexionComponent },
-  { path: 'DashBoard', component: PageDashBoardComponent },
+  {
+    path: 'DashBoard',
+    component: PageDashBoardComponent,
+    canActivate: [MonGuardGuard],
+  },
   { path: '**', component: PageConnexionComponent },
 ];
 
